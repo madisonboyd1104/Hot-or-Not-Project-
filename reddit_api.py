dfa2f9bd4efd1a254b11204b6b37646f8b63ffaf
madisonboyd1104 <madison.boyd@my.hamptonu.edu>
2025-11-11 15:49:32 -0500

    Add files via upload

--- reddit_api.py
+++ reddit_api.py
@@ -4,10 +4,13 @@ import sys
 from subreddit_query import subreddits , subreddit_query , iphone_keywords , samsung_keywords
 
 # === Function to Fetch Posts (Titles + Post Content) ===
-def fetch_posts(subreddit_query, keywords):
-    posts = []
-    for keyword in keywords:
-        for submission in subreddit_query.search(keyword, limit=10):
-            full_text = submission.title + " " + (submission.selftext[:500] if submission.selftext else "")
-            posts.append(full_text)
+def fetch_posts(subreddit_query, keywords,post_limit = 500):#Jared Johnson
+    posts = []#Jared Johnson
+    while(len(posts)<post_limit):#Jared Johnson
+        for keyword in keywords:#Jared Johnson
+            for submission in subreddit_query.search(keyword, limit=10):#Jared Johnson
+                full_text = submission.title + " " + (submission.selftext[:500] if submission.selftext else "")#Jared Johnson
+                posts.append(full_text)
+            
+        
     return posts
